4dd15cbb5267199b69a8d871b5f2506c21ce965e
woodwardem <woodwardem@g.crookcountyschools.org>
2021-10-01 08:45:22 -0700

    completed the classes lab

---
+++ classes.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+class Animal {
+    constructor(name, species, age) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+    getNameAndSpecies() {
+        let an = `${this.name} is the name of a ${this.species} dog`;
+        return an;
+    }
+};
+
+
+let Billy = new Animal("Billy", "German Sheapperd", 5);
+let Sam = new Animal("Sam", "Hampster", 2);
+let Darla = new Animal("Darla", "horse", 12);
+console.log(Billy);
+console.log(Darla);
+console.log("The name is ", Darla.name);
+console.log("Her species is ", Darla.species);
